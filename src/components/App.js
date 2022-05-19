d45534183859ca5edfdace8edc2cbd1c24ca492b
Alireza Talaei <alireza@Alirezas-MacBook-Pro.local>
2022-05-19 16:07:14 +1000

    Routes were added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-router-dom';
 import Events from './Events'
 import Users from './Users';
+import Subscribed from './Subscribed';
+
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <Events />
-        <Users />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Events />} />
+          <Route path='/user' element={<Users/>} />
+          <Route path='/subscribed' element={<Subscribed/>} />
+        </Routes>
+      </Router>
+        
     </div>
   );
 }
